d834dd47457b22c226f401277b16fec6e79ddcdf
tu.nm1 <ngominhtu237@gmail.com>
2018-07-17 01:12:52 +0700

    create custom Button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image, image } = album;
@@ -30,6 +31,10 @@ const AlbumDetail = ({ album }) => {
 
             <CardSection>
                 <Image style={imageStyle} source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = () => (
+    <TouchableOpacity>
+        <Text>Click me!!!</Text>
+    </TouchableOpacity>
+);
+
+export default Button;
+
