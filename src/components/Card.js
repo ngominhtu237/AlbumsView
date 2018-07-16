220c9da1baf3d8d715d800e2c67fb7b6139406cd
tu.nm1 <ngominhtu237@gmail.com>
2018-07-16 23:39:44 +0700

    First commit

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+// Mục đích là tách biệt CSS, ko phải viết chung trong AlbumDetail
+// 2 là tái sử dụng
+
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    );
+
+const styles = {
+    containerStyle: {
+        borderWidth: 1,
+        borderRadius: 2, 
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
+};
+
+export default Card;
